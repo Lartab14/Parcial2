039dc82513b6a923b0052be056da9b2b4ecd2930
Lartab14 <160176590+Lartab14@users.noreply.github.com>
2025-04-29 10:19:19 -0500

    pwa
    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Actualiza el Service Worker automáticamente cuando hay cambios
+      manifest: {
+        name: 'Futurama',
+        short_name: 'MiApp',
+        description: 'Una aplicación React convertida en PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Hace que la app se comporte como una app nativa
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/bender512.jpg',
+            sizes: '512x512',
+            type: 'image/jpg',
+          },
+          {
+            src: 'icons/bender128.jpg',
+            sizes: '128x128',
+            type: 'image/jpg',
+          },
+        ],
+      },
+      workbox: {
+        // Define qué archivos se cachean para que la app funcione offline
+        globPatterns: ['*/.{js,css,html,svg,png}'],
+      },
+    }),
+  ],
+});
